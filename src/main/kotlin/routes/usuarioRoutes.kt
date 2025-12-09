fc9dd26c827493fe26c0c7008188b6911bafe768
ferran <ferrancamposiniesta@gmail.com>
2025-12-09 17:09:54 +0100

    Iniciando routing en user

---
+++ src/main/kotlin/routes/usuarioRoutes.kt
@@ -0,0 +1,34 @@
+package org.example.routes
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.receive
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.example.models.DynamicQueryRequest
+
+import org.example.services.DynamicQueryService
+
+fun Route.userRoutes() {
+    route("/api/v1/user") {
+        post() {
+            try {
+                // Ktor deserializa automáticamente el cuerpo JSON en un objeto DynamicQueryRequest
+                val requestData = call.receive<DynamicQueryRequest>()
+                val results = service.executeQuery(requestData) // El servicio maneja la validación y la consulta
+                call.respond(results)
+            } catch (e: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest, mapOf("error" to e.message))
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.InternalServerError, mapOf("error" to "Error interno del servidor"))
+                e.printStackTrace()
+            }
+        }
+        put (){
+
+        }
+        delete() {
+
+        }
+    }
+}
