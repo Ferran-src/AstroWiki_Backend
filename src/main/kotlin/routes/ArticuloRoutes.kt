6953fd41a481ae9f51d96afbe8c6db5649895f39
LAZAR <andlaz2@a2-07.infor.caminas>
2026-01-16 19:04:52 +0100

    ArticuloRoutes refactorizado

--- src/main/kotlin/routes/ArticuloRoutes.kt
+++ src/main/kotlin/routes/ArticuloRoutes.kt
@@ -15,51 +15,82 @@ fun Route.articuloRoutes() {
     route("/articulos") {
 
         get {
-            call.respond(service.getAll())
-            //call.respondText("Hola")
-//            println("🔥 Entrando a GET /articulos")
-//            val lista = service.getAll()
-//            println("🔥 Resultado: $lista")
-//            call.respondText(lista.toString())
-        }
-
-        route("/articulos/{id}") {
-        get {
-            val id = call.parameters["id"]?.toIntOrNull()
-                ?: return@get call.respond(status = HttpStatusCode.BadRequest, "ID inválido")
-
-            val articulo = service.getById(id)
-                ?: return@get call.respond(status = HttpStatusCode.NotFound, "No encontrado")
-
-            call.respond(articulo.toString())
-        }
+            try {
+                call.respond(service.getAll())
+            } catch (e: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest, mapOf("error" to e.message))
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.InternalServerError, mapOf("error" to "Error interno del servidor"))
+                e.printStackTrace()
             }
 
-        post {
-            val articulo = call.receive<Articulo>()
-            val id = service.create(articulo)
-            call.respond(mapOf("id" to id))
         }
 
-        put("{id}") {
-            val id = call.parameters["id"]?.toIntOrNull()
-                ?: return@put call.respond(HttpStatusCode.BadRequest, "ID inválido")
+        route("/{id}") {
+            get {
+                try {
+                    val id = call.parameters["id"]?.toIntOrNull()
+                        ?: return@get call.respond(status = HttpStatusCode.BadRequest, "ID inválido")
 
-            val articulo = call.receive<Articulo>()
-            val ok = service.update(id, articulo)
+                    val articulo = service.getById(id)
+                        ?: return@get call.respond(status = HttpStatusCode.NotFound, "No encontrado")
 
-            if (ok) call.respond("Actualizado")
-            else call.respond(HttpStatusCode.NotFound, "No encontrado")
-        }
+                    call.respond(articulo.toString())
+                } catch (e: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest, mapOf("error" to e.message))
+                }
+                catch (e: Exception) {
+                    call.respond(HttpStatusCode.InternalServerError, mapOf("error" to "Error interno del servidor"))
+                    e.printStackTrace()
+                }
+            }
+            post {
+                try {
+                    val articulo = call.receive<Articulo>()
+                    val id = service.create(articulo)
+                    call.respond(mapOf("id" to id))
+                } catch (e: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest, mapOf("Error" to e.message))
+                } catch (e: Exception) {
+                    call.respond(HttpStatusCode.InternalServerError, mapOf("error" to "Error interno del servidor"))
+                    e.printStackTrace()
+                }
+            }
+            put {
+                try {
+                    val id = call.parameters["id"]?.toIntOrNull()
+                        ?: return@put call.respond(HttpStatusCode.BadRequest, "ID inválido")
 
-        delete("/delete/{id}") {
-            val id = call.parameters["id"]?.toIntOrNull()
-                ?: return@delete call.respond(HttpStatusCode.BadRequest, "ID inválido")
+                    val articulo = call.receive<Articulo>()
+                    val ok = service.update(id, articulo)
 
-            val ok = service.delete(id)
+                    if (ok) call.respond("Actualizado")
+                    else call.respond(HttpStatusCode.NotFound, "No encontrado")
+                } catch (e: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest, mapOf("Error" to e.message))
+                }
+                catch (e: Exception) {
+                    call.respond(HttpStatusCode.InternalServerError, mapOf("error" to "Error interno del servidor"))
+                    e.printStackTrace()
+                }
+            }
 
-            if (ok) call.respond("Marcado como borrado")
-            else call.respond(HttpStatusCode.NotFound, "No encontrado")
+            delete {
+                try {
+                    val id = call.parameters["id"]?.toIntOrNull()
+                        ?: return@delete call.respond(HttpStatusCode.BadRequest, "ID inválido")
+
+                    val ok = service.delete(id)
+
+                    if (ok) call.respond("Marcado como borrado")
+                    else call.respond(HttpStatusCode.NotFound, "No encontrado")
+                } catch (e: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest, mapOf("error" to e.message))
+                } catch (e: Exception) {
+                    call.respond(HttpStatusCode.InternalServerError, mapOf("error" to "Error interno del servidor"))
+                    e.printStackTrace()
+                }
+            }
         }
     }
 }
